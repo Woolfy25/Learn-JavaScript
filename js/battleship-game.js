184a862fdda91191119d64bcb8d57e5c3f91dc91
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-04-04 22:55:29 +0300

    battleship game v1

---
+++ js/battleship-game.js
@@ -0,0 +1,26 @@
+let view = {
+  displayMessage: function (msg) {
+    let messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+
+  displayHit: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+
+  displayMiss: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
+};
+
+//TESTING
+view.displayMiss("66");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("Tap tap, is this thing on?");
