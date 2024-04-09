72c484f809cb9c52c85378332795d1bbcfdb4bac
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-04-09 22:24:51 +0300

    test-exercise

---
+++ js/test-goit.js
@@ -0,0 +1,12 @@
+function calculateTotal(number) {
+  // Change code below this line
+  let sum = 0;
+  for (let i = 0; i <= number; i++) {
+    sum += i;
+  }
+
+  return sum;
+
+  // Change code above this line
+}
+console.log(calculateTotal(3));
