8e578dadcf953fa0b990b4253674674e324b664d
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-04-10 15:57:16 +0300

    test-exercise

--- js/test-goit.js
+++ js/test-goit.js
@@ -1,12 +1,43 @@
+// EXERCISE 1
 function calculateTotal(number) {
-  // Change code below this line
   let sum = 0;
   for (let i = 0; i <= number; i++) {
     sum += i;
   }
 
   return sum;
-
-  // Change code above this line
 }
-console.log(calculateTotal(3));
+// console.log(calculateTotal(3));
+
+// EXERCISE 2
+
+function findLongestWord(string) {
+  // Change code below this line
+  let splitString = string.split(" ");
+  let longest = "";
+  console.log(splitString);
+
+  for (let i = 0; i < splitString.length; i++) {
+    if (splitString[i].length > longest.length) {
+      longest = splitString[i];
+    }
+  }
+  return longest;
+}
+
+// console.log(findLongestWord("May the force be with you"));
+
+// EXERCISE 3
+
+function filterArray(numbers, value) {
+  let result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      result.push[numbers[i]];
+    }
+  }
+  return result;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 5));
