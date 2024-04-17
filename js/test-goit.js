91ff22f3ecb0e8051a06bab45840e2e433a1b1a9
Ciutre Ramon <80210853+Woolfy25@users.noreply.github.com>
2024-04-17 22:02:39 +0300

    testing-goit-exercises

--- js/test-goit.js
+++ js/test-goit.js
@@ -40,4 +40,75 @@ function filterArray(numbers, value) {
   return result;
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+
+// EXERCISE 4
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+
+for (let value of colors) {
+  if ((value = "rgb")) {
+    rgbColors.push(colors[value]);
+  } else {
+    hexColors.push(colors[value]);
+  }
+}
+
+// console.log(rgbColors);
+// console.log(hexColors);
+
+// EXERCISE 5
+
+const products1 = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  let productPrice;
+
+  for (let objects of products1) {
+    if (objects.name === productName) {
+      return (productPrice = objects.price);
+    } else {
+      return (productPrice = null);
+    }
+  }
+}
+
+// console.log(getProductPrice("Scanner"));
+
+// EXERCISE 6
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+  let result = [];
+
+  for (let objects of products) {
+    if (Objects.keys(objects) === propName) {
+      return 100000;
+    }
+  }
+  return result;
+
+  // Change code above this line
+}
+
+console.log(getAllPropValues("name"));
